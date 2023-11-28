f884d729c6fe07aad334f6dc2751f17d2dd63568
Guilherme Costa <gui-costaquino@hotmail.com>
2023-11-28 09:07:17 -0300

    Utilizando mongoDB pelo container docker

--- src/database/db.js
+++ src/database/db.js
@@ -1,19 +1,15 @@
-const mongoose = require('mongoose')
-require('dotenv').config()
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", true);
 
-const DB_USER = process.env.DB_USER
-const DB_PASSWORD = process.env.DB_PASSWORD
-const DB_NAME = process.env.DB_NAME
-const DB_CODE = process.env.DB_CODE
-
-const connectMongoDB = async function (){
-  mongoose.set('strictQuery', true)
-  mongoose.Promise = global.Promise
-  mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_NAME}.${DB_CODE}.mongodb.net/?retryWrites=true&w=majority`)
-    .then(() =>{
-        useMongoClient =  true;
-        console.log('conectou ao MongoDB!')
-    }).catch((err) => console.log(err))
+const connectMongoDB = () => {
+  mongoose
+    .connect(
+      `mongodb://127.0.0.1:27017/mobile`
+    )
+    .then(() => {
+      console.log("Conectou ao banco!");
+    })
+    .catch((err) => console.log(err));
 }
 
 module.exports = {ConnectMongoDB: connectMongoDB}
