c21dd839b82b9f35471d0341cb784c1269f7be6c
Guilherme Costa <gui-costaquino@hotmail.com>
2023-11-28 10:29:17 -0300

    Tentando renderizar view que contém a página de login com o google

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
-const express = require('express')
-const app = express()
-const bookController = require('./src/controllers/commentController')
-const db = require('./src/database/db')
+const express = require("express");
+const app = express();
+const bookController = require("./src/controllers/commentController");
+const db = require("./src/database/db");
+const { handlebars, engine } = require("express-handlebars");
+const path = require('path');
 
-app.use(express.json())
-db.ConnectMongoDB()
+db.ConnectMongoDB();
 
-app.post('/criar', (req, res) => bookController.create(req, res))
-app.get('/comentarios', (req, res) => bookController.commentAll(req, res))
-app.delete('/deletar/:id', (req, res) => bookController.deleteComment(req, res))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname + "src/views")));
+
+
+// Handlebars
+app.engine("handlebars", engine({ layout: false }));
+app.set("view engine", "handlebars");
+
+app.get("/", (req, res) => bookController.renderMain(req, res));
+app.post("/criar", (req, res) => bookController.create(req, res));
+app.get("/comentarios", (req, res) => bookController.commentAll(req, res));
+app.delete("/deletar/:id", (req, res) =>
+  bookController.deleteComment(req, res)
+);
 
 app.listen(3000, () => {
-  console.log('servidor rodando porta 3000')
-})
+  console.log("servidor rodando porta 3000");
+});
