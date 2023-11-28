1d858e0dfaf45d8025910976b1c4f8b35b4af403
leomendes18 <leomendesmonteiro1@gmail.com>
2023-11-28 09:13:59 -0300

    mudando connect na database mongo

--- src/database/db.js
+++ src/database/db.js
@@ -3,13 +3,11 @@ mongoose.set("strictQuery", true);
 
 const connectMongoDB = () => {
   mongoose
-    .connect(
-      `mongodb://127.0.0.1:27017/mobile`
-    )
+    .connect(`mongodb://127.0.0.1:27017/booksApi`)
     .then(() => {
       console.log("Conectou ao banco!");
     })
     .catch((err) => console.log(err));
-}
+};
 
-module.exports = {ConnectMongoDB: connectMongoDB}
+module.exports = { ConnectMongoDB: connectMongoDB };
