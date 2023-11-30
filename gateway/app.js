533c6771a18db4b380e697ae5e516ed9b34a2c1e
leomendes18 <leomendesmonteiro1@gmail.com>
2023-11-29 21:48:28 -0300

    separando as apis gateway e comments

---
+++ gateway/app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const { handlebars, engine } = require("express-handlebars");
+const path = require("path");
+const routes = require("./src/routes/routes");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+
+app.use(express.static("./src"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  session({
+    secret: "dsaddssd",
+    name: "sessionId",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 7 * 24 * 60 * 60 * 1000 },
+  })
+);
+
+// Handlebars
+app.engine("handlebars", engine({ layout: false }));
+app.set("views", __dirname + "/src/views");
+app.set("view engine", "handlebars");
+app.use(routes);
+
+app.listen(3000, () => {
+  console.log("servidor rodando porta 3000");
+});
---
+++ gateway/src/controllers/controller.js
@@ -0,0 +1,17 @@
+async function renderMain(req, res) {
+  try {
+    res.render("partials/initial", { layout: "main" });
+  } catch (err) {
+    console.log("ERRO: " + err);
+  }
+}
+
+async function renderBooks(req, res) {
+  try {
+    res.render("partials/books", { layout: "main" });
+  } catch (err) {
+    console.log("ERRO: " + err);
+  }
+}
+
+module.exports = { create, commentAll, deleteComment, renderMain, renderBooks };
