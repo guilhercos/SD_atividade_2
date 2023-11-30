8a6ebcf3f97c4037847a13b4cc35745f516509af
leomendes18 <leomendesmonteiro1@gmail.com>
2023-11-30 18:10:07 -0300

    fazendo o signin de gateway

--- gateway/src/controllers/controller.js
+++ gateway/src/controllers/controller.js
@@ -1,3 +1,5 @@
+const axios = require("axios");
+
 async function renderMain(req, res) {
   try {
     res.render("partials/initial", { layout: "main" });
@@ -16,6 +18,16 @@ async function renderBooks(req, res) {
 
 async function signin(req, res) {
   const { credential } = req.body;
+
+  const getAxios = axios.post("http://localhost:6000");
+  const token = getAxios.data.token;
+  const user = getAxios.data.user;
+
+  const tokenBearer = `Bearrer ${token}`;
+  req.session.user = user;
+
+  res.cookie("acess_token", tokenBearer, { maxAge: 3600000 });
+  res.set("Authorization", tokenBearer);
 }
 
 module.exports = { renderMain, renderBooks };
